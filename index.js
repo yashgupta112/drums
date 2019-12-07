bc1bcd3725ce8aa8853b4a276562d53a418e25c0
yashgupta112 <58629996+yashgupta112@users.noreply.github.com>
2019-12-07 19:21:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -42,32 +42,32 @@ document.addEventListener('keypress', function(e){
 
 function tom1() {
 
-   var x = new Audio("sounds/tom-1.mp3");
+   var x = new Audio("tom-1.mp3");
    x.play();
 
 }
 function tom2() {
-  var x = new Audio("sounds/tom-2.mp3");
+  var x = new Audio("tom-2.mp3");
 x.play();
 }
 function tom3() {
-  var x = new Audio("sounds/tom-3.mp3");
+  var x = new Audio("tom-3.mp3");
   x.play();
 
 }
 function tom4() {
-  var x = new Audio("sounds/tom-4.mp3");
+  var x = new Audio("tom-4.mp3");
   x.play();
 }
 function snare() {
-  var x = new Audio("sounds/snare.mp3");
+  var x = new Audio("snare.mp3");
   x.play();
 }
 function crash() {
-  var x = new Audio("sounds/crash.mp3");
+  var x = new Audio("crash.mp3");
   x.play();
 }
 function kick() {
-  var x = new Audio("sounds/kick-bass.mp3");
+  var x = new Audio("kick-bass.mp3");
   x.play();
 }
